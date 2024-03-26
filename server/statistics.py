936f0fdad31aae340d3b3bea08daac63f1610a97
luisarce02 <luis.arce.60088@usalesiana.edu.bo>
2024-03-26 08:31:18 -0400

    Update statistics.py

--- server/statistics.py
+++ server/statistics.py
@@ -1,13 +1,12 @@
 import requests
-
+from peripheral.constants import WIDTH, HEIGHT
 def send_post_request(pointer_position):
     url = "http://127.0.0.1:8000/results/create"
     
     sample_data = {
-        "id": "test_id",
-        "name": "Sample Name",
-        "x": pointer_position[0],
-        "y": pointer_position[1],
+        "name": "Shoot Example Joycon Position",
+        "x": min(max((pointer_position[0] * WIDTH / 2 + WIDTH / 2), 0), WIDTH),
+        "y": min(max(int(-pointer_position[1] * HEIGHT / 2 + HEIGHT / 2), 0), HEIGHT),
         "scenary": {
             "bullet_weight": 5.0,
             "distance": 100.0,
@@ -23,7 +22,7 @@ def send_post_request(pointer_position):
 
     response = requests.post(url, json=sample_data)
     
-    if response.status_code == 201:
+    if response.status_code == 200:
         print("Success.")
     else:
         print(f"Error in request POST: {response.status_code}")
